749cacac401fe8e8e8f872b30b54be6f75625f80
XAN012 <133760910+XAN012@users.noreply.github.com>
2025-02-28 19:24:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def download():
     return jsonify({"status": "Downloaded", "file": f"{song_name}.mp3"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=8080)
